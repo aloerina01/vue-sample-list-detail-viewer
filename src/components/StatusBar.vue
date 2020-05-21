9caf5067612b66f56db2af403b26cb206e2722dd
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2020-05-21 17:39:18 +0900

    Implement StatusBar component

---
+++ src/components/StatusBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'StatusBar',
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  background-color: #004ba0;
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 16px;
+  border-radius: 16px 16px 0 0;
+}
+@media screen and (max-width: 436px) {
+  div {
+    border-radius: 0;
+  }
+}
+</style>
