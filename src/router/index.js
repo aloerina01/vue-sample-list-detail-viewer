097230b86d9e51814b4cfcb23a98a0cc46204bdb
aloerina01 <4443321+aloerina01@users.noreply.github.com>
2020-06-02 19:07:57 +0900

    Implement navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import ListPage from '../pages/ListPage';
 import DetailPage from '../pages/DetailPage';
+import { ItemsStoreQuery } from '../queries';
 
 Vue.use(VueRouter);
 
@@ -11,6 +12,13 @@ const router = new VueRouter({
     { path: '/detail/:itemId', component: DetailPage, props: true },
     { path: '*', redirect: '/list' },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.path !== '/list' && ItemsStoreQuery.items.length === 0) {
+    return next('/list');
+  }
+  next();
 });
 
 export { router };
